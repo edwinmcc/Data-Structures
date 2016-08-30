6f94cb1a0fccaa25a46c0d8f4fbc95a4c41c2cfb
edwinmcc <edwinmcc@gmail.com>
2016-08-30 22:19:27 +1000

    Added BiFunctionDemo

---
+++ src/edu/learn/java/jdk8/BiFunctionDemo.java
@@ -0,0 +1,17 @@
+package edu.learn.java.jdk8;
+
+/**
+ * Created by edwin on 30/08/16.
+ */
+@FunctionalInterface
+interface BiFunction<T,U,V> {
+    V apply(T t, U u);
+}
+
+
+
+public class BiFunctionDemo {
+
+
+
+}
