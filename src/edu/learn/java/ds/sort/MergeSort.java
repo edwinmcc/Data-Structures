5e4305838786adf029a49c0e78db91b350d413b6
edwinmcc <edwinmcc@gmail.com>
2016-09-13 07:16:38 +1000

    Fixed issues in Merge Sort

--- src/edu/learn/java/ds/sort/MergeSort.java
+++ src/edu/learn/java/ds/sort/MergeSort.java
@@ -106,35 +106,41 @@ public class MergeSort {
     int[] array = { 9,8,7,6,5,4,3,2,1, 0};
 
     public void merge(int low, int mid, int high) {
+		//System.out.printf("merge(%d, %d, %d) \n",low,mid,high);
 
         // Compute the size of array to split the main array.
         int leftArraySize = mid - low + 1;
         int rightArraySize = high - mid;
 
-        int[] leftArray = new int[leftArraySize];
-        int[] rightArray = new int[rightArraySize];
+		//System.out.printf("Left Half Size : %d Right Half size : %d ",leftArraySize,rightArraySize);
+
+        int[] leftArray = new int[leftArraySize + 1];
+        int[] rightArray = new int[rightArraySize +1];
 
         // Fill the array by copying elements from original array.
         for(int i=0;i<leftArraySize;i++) {
-            leftArray[i] = array[i];
+            leftArray[i] = array[low + i];
         }
 
         for(int i=0;i<rightArraySize;i++) {
-            rightArray[i] = array[mid+i];
+            rightArray[i] = array[mid+i+1];
         }
 
         leftArray[leftArraySize]=Integer.MAX_VALUE;
-        rightArray[high] = Integer.MAX_VALUE;
+        rightArray[rightArraySize] = Integer.MAX_VALUE;
 
         int i=0;
         int j=0;
 
-        for(int k = low;k<high;k++) {
-            if(leftArray[i] < rightArray[j]) {
+        for(int k = low;k<=high;k++) {
+        	//System.out.printf("Comparing leftArray[%d](%d)<=rightArray[%d](%d) \n",i,leftArray[i],j,rightArray[j]);
+            if(leftArray[i] <= rightArray[j]) {
+				//System.out.printf("array[%d](%d) = leftArray[%d](%d) \n",k,array[k],i,leftArray[i]);
                 array[k] = leftArray[i];
                 i++;
             }
             else {
+				//System.out.printf("array[%d](%d) = rightArray[%d](%d) \n",k,array[k],j,rightArray[j]);
                 array[k] = rightArray[j];
                 j++;
             }
@@ -142,6 +148,7 @@ public class MergeSort {
     }
 
     public void mergeSort(int low, int high) {
+    	//System.out.printf("mergeSort(%d,%d) \n",low,high);
         if(low<high) {
             int mid = (low + high) / 2;
             mergeSort(low, mid);
@@ -157,7 +164,7 @@ public class MergeSort {
     public static void main(String ...args) {
         MergeSort ms=new MergeSort();
         ms.printArray();
-        ms.mergeSort(0,ms.array.length);
+        ms.mergeSort(0,ms.array.length-1);
         ms.printArray();
     }
 }
