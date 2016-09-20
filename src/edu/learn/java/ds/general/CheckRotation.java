94090f415ffd6c9881889b91f96e4cad624b96ad
edwinmcc <edwinmcc@gmail.com>
2016-09-21 07:00:19 +1000

    Check Rotation

---
+++ src/edu/learn/java/ds/general/CheckRotation.java
@@ -0,0 +1,31 @@
+package edu.learn.java.ds.general;
+
+/**
+ * Created by edwin on 21/09/16.
+ */
+public class CheckRotation {
+
+    private boolean isSubString(String s1, String s2) {
+        return s2.contains(s1);
+    }
+
+    public boolean isRotation(String s1, String s2) {
+        if(s1==null || s2==null) {
+            return false;
+        }
+        if(s1.length()!=s2.length()) {
+            return false;
+        }
+        String bigString=s2+s2;
+        return isSubString(s1,bigString);
+    }
+
+
+    public static void main(String ...args) {
+        CheckRotation cr=new CheckRotation();
+        String s1="apple";
+        String s2="ppale";
+        System.out.println(" Is Rotation : "+cr.isRotation(s1,s2));
+    }
+
+}
