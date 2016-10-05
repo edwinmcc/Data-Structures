9e9545f2efb76257ce9b52fe315dc9b6b4d108a6
edwinmcc <edwinmcc@gmail.com>
2016-10-05 13:40:47 +1100

    Updated some comments

--- src/edu/learn/java/ds/general/MaximumPerimeterTriangle.java
+++ src/edu/learn/java/ds/general/MaximumPerimeterTriangle.java
@@ -1,6 +1,7 @@
 package edu.learn.java.ds.general;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -78,6 +79,21 @@ class Triangle {
         return s3;
     }
 
+    public int getMinSide() {
+        if (s1 < s2) {
+            if(s1<s3) {
+                return s1;
+            }
+        } else if (s2 < s3) {
+            return s2;
+        }
+        return s3;
+    }
+
+    public int getPerimeter() {
+        return s1+s2+s3;
+    }
+
     public String toString() {
         return String.format("%d-%d-%d",s1,s2,s3);
     }
@@ -86,9 +102,46 @@ class Triangle {
 public class MaximumPerimeterTriangle {
 
     private List<Triangle> triangleList=new ArrayList<Triangle>();
+    private List<Triangle> longMaxSideList=null;
+    private List<Triangle> longMinSideList=null;
 
     public List<Triangle> getTriangleList() {
         return triangleList;
+    }
+
+
+    private void processLongestMaximumSide(Triangle triangle) {
+        if(longMaxSideList==null) {
+            longMaxSideList = new ArrayList<Triangle>();
+            longMaxSideList.add(triangle);
+        }
+        else {
+            Triangle triangle1=longMaxSideList.get(0);
+            if(triangle.getMaxSide()>triangle1.getMaxSide()) {
+                longMaxSideList.clear();
+                longMaxSideList.add(triangle);
+            }
+            else if(triangle.getMaxSide()==triangle1.getMaxSide()) {
+                longMaxSideList.add(triangle);
+            }
+        }
+    }
+
+    private void processLongestMinimumSide(Triangle triangle) {
+        if(longMinSideList==null) {
+            longMinSideList = new ArrayList<Triangle>();
+            longMinSideList.add(triangle);
+        }
+        else {
+            Triangle triangle1=longMinSideList.get(0);
+            if(triangle.getMinSide()>triangle1.getMinSide()) {
+                longMinSideList.clear();
+                longMinSideList.add(triangle);
+            }
+            else if(triangle.getMinSide()==triangle1.getMinSide()) {
+                longMinSideList.add(triangle);
+            }
+        }
     }
 
     private void makeTriangle(ArrayList<Integer> prefix, ArrayList<Integer> suffix) {
@@ -96,9 +149,10 @@ public class MaximumPerimeterTriangle {
         if(suffix.size()==3) {
             Triangle triangle=new Triangle(suffix.get(0),suffix.get(1),suffix.get(2));
             triangleList.add(triangle);
+            processLongestMaximumSide(triangle);
+            processLongestMinimumSide(triangle);
         }
         else {
-
             for(int i=0;i<n;i++) {
                 ArrayList<Integer> prefixList=new ArrayList<Integer>(prefix);
                 prefixList.add(suffix.get(i));
@@ -126,6 +180,14 @@ public class MaximumPerimeterTriangle {
 
         for(int i=0;i<triangleList.size();i++) {
             System.out.println("Triangle : "+triangleList.get(i));
+        }
+
+        for(int i=0;i<mpt.longMaxSideList.size();i++) {
+            System.out.println("Triangle Max : "+mpt.longMaxSideList.get(i));
+        }
+
+        for(int i=0;i<mpt.longMinSideList.size();i++) {
+            System.out.println("Triangle Min : "+mpt.longMinSideList.get(i));
         }
 
         /*Triangle t1=new Triangle(scanner.nextInt(),scanner.nextInt(),scanner.nextInt());
